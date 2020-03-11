d77611ec718c9bf53aca518c71904fddd6bf5c6d
Алексей <avsh24@yandex.ru>
2020-03-11 14:31:50 +0500

    0. Что такое переменная.[#253055]

---
+++ src/ru/job4j/DataType.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class DataType {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+    }
+}
