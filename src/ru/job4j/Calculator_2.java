fc3ac7d6943102591d3274f77b1de13dbcfb1d33
avsh24 <43810524+avsh24@users.noreply.github.com>
2020-03-11 20:58:43 +0500

    Delete Calculator_2.java

--- src/ru/job4j/Calculator_2.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class Calculator_2 {
-    public static void main(String[] args) {
-        int size = 10;
-        size = 100 - 5;
-        System.out.println(size);
-    }
-}
