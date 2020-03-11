2b5a51474f11eb08f270b23cd21e3cd0a0a7d886
Алексей <avsh24@yandex.ru>
2020-03-11 18:56:48 +0500

    1. Определите тип данных для переменных.[#253046]

---
+++ src/ru/job4j/PrDataType.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class PrDataType {
+    public static void main(String[] args) {
+        int age; //Возраст
+        boolean gender; //Пол
+        double growth; //Рост
+        long bricks; //количество кирпичей для постройки дома
+        long bacteria; //количество бактерий
+        double distance; //расстояние до космических объектов
+        boolean permission; //разрешение на работу
+        long file; //размер файла
+    }
+}
