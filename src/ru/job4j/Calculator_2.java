76f004d29c3046687cf4aec839221c3be331f75a
Алексей <avsh24@yandex.ru>
2020-03-11 20:55:30 +0500

    3. Арифметические операции.[#253053]

---
+++ src/ru/job4j/Calculator_2.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Calculator_2 {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100 - 5;
+        System.out.println(size);
+    }
+}
