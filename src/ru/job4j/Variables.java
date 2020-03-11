c89cf4efb88395a1138d9dc1d88b68417e0115f9
Алексей <avsh24@yandex.ru>
2020-03-11 19:12:26 +0500

    2. Переменные.[#253047]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18; //Возраст
+        boolean gender = false; //Пол
+        double growth = 1.70; //Рост
+        long bricks = 2147483648L; //количество кирпичей для постройки дома
+        long bacteria = 2147483649L; //количество бактерий
+        double distance = 4.9e-324; //расстояние до космических объектов
+        boolean permission = false; //разрешение на работу
+        long file = 2147483649L; //размер файла
+    }
+}
