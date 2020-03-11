4a4271feb149f83c5b857dfbe9d89b8265f78fa5
Алексей <avsh24@yandex.ru>
2020-03-11 13:28:32 +0500

    3.3. Разбор первой программы.[#253061]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Алексей Шемякин");
+        System.out.println("24.05.1977");
+    }
+}
