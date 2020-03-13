974deb72f88fb8f6fae5fae9f00992f805a125ce
Алексей <avsh24@yandex.ru>
2020-03-13 19:03:32 +0500

    5. Конвертер валюты[#253041] 0. Что такое тестирование.[#253095]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -6,7 +6,7 @@ public class Converter {
         return rsl;
     }
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; // формула перевода рублей в доллоры.
+        int rsl = value / 60; // формула перевода рублей в доллары.
         return rsl;
     }
     public static void main(String[] args) {
@@ -14,5 +14,19 @@ public class Converter {
         int dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("180 rubles are " + dollar + " dollar. ");
+
+        // Тест перевода рублей в евро.
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        // Тест перевода рублей в доллары.
+        int in1 = 180;
+        int expected1 = 3;
+        int out1 = rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("180 rubles are 3. Test result : " + passed1);
     }
 }
