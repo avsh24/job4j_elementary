64eaf7e25dc957b32fdec181a4e0c1dad4044eec
Алексей <avsh24@yandex.ru>
2020-03-13 13:26:52 +0500

    4. Методы[#253049]

--- src/ru/job4j/calculator/Calculator3.java
+++ src/ru/job4j/calculator/Calculator3.java
@@ -1,10 +1,10 @@
 package ru.job4j.calculator;
 
-public class Calculator_3 {
+public class Calculator3 {
     public static void main(String[] args) {
-        Calculator_3.plus(100, 500);
-        Calculator_3.plus(4, 2);
-        Calculator_3.plus(3, 5);
+        Calculator3.plus(100, 500);
+        Calculator3.plus(4, 2);
+        Calculator3.plus(3, 5);
     }
     public static void plus(int first, int second) {
         int result = first + second;
