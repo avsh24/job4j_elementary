7001dc6d853dbfd0b58e74b3e0947b7227708eef
Алексей <avsh24@yandex.ru>
2020-03-28 21:50:11 +0500

    5.8. JavaDoc. Code convention[#253092]

--- src/ru/job4j/calculator/Calculator1.java
+++ src/ru/job4j/calculator/Calculator1.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculator;
-
+/**
+ * Calculator - class for calculating arithmetic operations.
+ *
+ *@author Aleksey Shemyakin (avsh24@yandex.ru)
+ *@version 1
+ *@since 28.03.2020
+ */
 public class Calculator1 {
+    /**
+     * result of the operation.
+     */
     public static void main(String[] args) {
         int two = 2;
         int six = 6;
