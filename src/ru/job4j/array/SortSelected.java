69950a35140930f3dafd218caaaead141852a8cb
Алексей <avsh24@yandex.ru>
2020-04-07 14:41:00 +0500

    6.5.3. Сортировка выборкой.[#253024]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,13 +1,13 @@
 package ru.job4j.array;
 
-import static com.sun.tools.javac.jvm.ByteCodes.swap;
-
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, 0, data.length);
             int index = FindLoop.indexOf(data, min, 0, data.length);
-            swap();
+            int temp = data[i];
+
+
         }
         return data;
     }
