a833fa7a573f60324b55e1443149aea48580433f
Алексей <avsh24@yandex.ru>
2020-04-09 13:32:46 +0500

    6.7.1. Моно строка в матрице.[#253027]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop.indexOf(data, min, i, data.length);
             int temp = data[index];
             data[index] = data[min];
             data[min] = temp;
