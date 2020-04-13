86d9ad490b3da26bc499e89cbbf77a3a243ed9bc
Алексей <avsh24@yandex.ru>
2020-04-13 21:30:05 +0500

    1.6. Взаимодействие объектов.[#252971]

---
+++ src/ru/job4j/oop/BallStory.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+
+        hare.tryEat(ball);
+        ball.talk();
+        wolf.tryEat(ball);
+        ball.talk();
+        fox.tryEat(ball);
+        ball.talk();
+        fox.kill(ball);
+    }
+}
