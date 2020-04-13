063fc4d42463ae1aeca29fbe08a399cea58d0d47
Алексей <avsh24@yandex.ru>
2020-04-13 13:13:08 +0500

    6.7.4. Выигрышные комбинации в сокобан[#253020]

--- src/ru/job4j/array/MatrixCheckTest.java
+++ src/ru/job4j/array/MatrixCheckTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MatrixCheckTest {
+
     @Test
     public void whenHasMonoHorizontal() {
         char[][] input = {
