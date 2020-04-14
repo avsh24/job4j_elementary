b61614c96eb23fe5ca9e373baaed858f7b1378fb
Алексей <avsh24@yandex.ru>
2020-04-14 22:27:00 +0500

    2.2. Рефакторинг - Площадь треугольника.[#252964]

---
+++ src/ru/job4j/oop/Triangle.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+    /**
+     * Метод должен вычислить площадь треугольника.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     */
+    public boolean exist(double a, double b, double c) {
+            return (a < b + c && b < a + c && c < a + b);
+    }
+}
