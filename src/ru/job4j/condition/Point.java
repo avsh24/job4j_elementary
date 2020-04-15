161e4ed2915f2b34dc0a456013e97cff5a5e6e55
Алексей <avsh24@yandex.ru>
2020-04-15 18:06:41 +0500

    4. Расстояние между точками в трехмерном пространстве.[#252965]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,19 +1,45 @@
 package ru.job4j.condition;
 
+import static java.lang.StrictMath.pow;
+import static java.lang.StrictMath.sqrt;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+    private int x;
+    private int y;
+    private int z;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+//    public static double distance(int x1, int y1, int x2, int y2) {
+//        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+//        return rsl;
+//    }
+//    public static void main(String[] args) {
+//        double result = Point.distance(0, 0, 2, 0);
+//        System.out.println("result (0, 0) to (2, 0) " + result);
 
         // Тест для проверки расстояний между точками в системе координат.
-        double in = 2.0;
-        double expected = 1.4142135623730951;
-        double out = Math.sqrt(in);
-        boolean passed = expected == out;
-        System.out.println("result (0, 0) to (2, 0) " + passed);
-    }
+//        double in = 2.0;
+//        double expected = 1.4142135623730951;
+//        double out = Math.sqrt(in);
+//        boolean passed = expected == out;
+//        System.out.println("result (0, 0) to (2, 0) " + passed);
+//    }
 }
--- src/ru/job4j/condition/PointTest.java
+++ src/ru/job4j/condition/PointTest.java
@@ -1,18 +1,31 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PointTest {
-    // Тест для проверки расстояний между точками в системе координат.
+
     @Test
-    public void distance() {
-        double x1 = 0;
-        double y1 = 0;
-        double x2 = 2;
-        double y2 = 0;
-        double expected = 2.0;
-        double out = Point.distance(0, 0, 2, 0);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point first = new Point(0, 0);
+        Point second = new Point(3, 0);
+        Point third = new Point(0, 4);
+        Triangle triangle = new Triangle(first, second, third);
+        double result = triangle.area();
+        double expected = 6D;
+        assertEquals(result, expected, 0.01);
     }
+
+    // Тест для проверки расстояний между точками в системе координат.
+//    @Test
+//    public void distance() {
+//        double x1 = 0;
+//        double y1 = 0;
+//        double x2 = 2;
+//        double y2 = 0;
+//        double expected = 2.0;
+//        double out = Point.distance(0, 0, 2, 0);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
 }
