a97ab45358c8e89c4009a3f0f9b94634ee7c48d7
Алексей <avsh24@yandex.ru>
2020-04-15 18:08:20 +0500

    3. Перегрузить метод max для трех чисел[#252966]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,7 +1,42 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-            return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+    /**
+     * Метод должен вычислить площадь треугольника.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     */
+    public boolean exist(double a, double b, double c) {
+        return (a < b + c && b < a + c && c < a + b);
+    }
+//    public static boolean exist(double ab, double ac, double bc) {
+//            return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+//    }
 }
--- src/ru/job4j/condition/TriangleTest.java
+++ src/ru/job4j/condition/TriangleTest.java
@@ -1,14 +1,35 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
 
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point first = new Point(0, 0);
+        Point second = new Point(3, 0);
+        Point third = new Point(0, 4);
+        Triangle triangle = new Triangle(first, second, third);
+        double result = triangle.area();
+        double expected = 6D;
+        assertEquals(result, expected, 0.01);
     }
+
+//    @Test
+//    public void whenAreaSetThreePointsThenTriangleArea() {
+//        Point a = new Point(0, 0);
+//        Point b = new Point(0, 2);
+//        Point c = new Point(2, 0);
+//        ru.job4j.oop.Triangle triangle = new Triangle(a, b, c);
+//        double result = triangle.area();
+//        assertThat(result, closeTo(2.0, 2.0));
+//    }
+
+//    @Test
+//    public void whenExist() {
+//        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+//        assertThat(result, is(true));
+//    }
 }
