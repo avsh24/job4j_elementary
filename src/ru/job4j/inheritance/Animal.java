310832cc0d6b6ede73fc157e502bf60bcbc16070
Алексей <avsh24@yandex.ru>
2020-04-16 11:57:58 +0500

    1. Конструктор по умолчанию.[#252978]

---
+++ src/ru/job4j/inheritance/Animal.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+        System.out.println("Animal");
+    }
+
+    public static void main(String[] args) {
+        Predator predator = new Predator("Predator");
+        Tiger tiger = new Tiger("Tiger");
+    }
+}
---
+++ src/ru/job4j/inheritance/Tiger.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class Tiger extends Animal {
+    public Tiger(String name) {
+        super(name);
+        System.out.println("Tiger");
+    }
+}
