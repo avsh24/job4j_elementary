003ead5d1e0d4d0fca3a71ff8cfbebf36776ca8e
Алексей <avsh24@yandex.ru>
2020-04-18 20:12:22 +0500

    4. Переопределение[#252980]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -2,4 +2,8 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    public String generate(String name, String body) {
+        String nl = System.lineSeparator();
+        return String.format("{%s name: %s%s, body: %s%s}", nl, name, nl, body, nl);
+    }
 }
--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class ReportUsage {
+
     public static void main(String[] args) {
         HtmlReport report = new HtmlReport();
         String text = report.generate("Report's name", "Report's body");
