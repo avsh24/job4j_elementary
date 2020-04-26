d564729cfbe8fbaa39f9c463d54a17aa82a7a40a
Алексей <avsh24@yandex.ru>
2020-04-26 20:40:49 +0500

    7. Шаблон проектирования - Стратегия.[#252940]

---
+++ src/ru/job4j/poly/stragery/Paint.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly.stragery;
+
+public class Paint {
+    public static void main(String[] args) {
+        draw(new Triangle());
+        draw(new Square());
+    }
+    public static void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
