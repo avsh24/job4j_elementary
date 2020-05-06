45f998651ec3fb72e2872cf33605f40a1f23ea96
Алексей <avsh24@yandex.ru>
2020-05-06 22:28:37 +0500

    2.1. Реализация класса StartUI[#252938]

--- src/ru/job4j/encapsulation/tracker/StartUI.java
+++ src/ru/job4j/encapsulation/tracker/StartUI.java
@@ -1,13 +1,43 @@
 package ru.job4j.encapsulation.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
-    public static void main(String[] args) {
-        Input input = new ConsoleInput();
-        Tracker tracker = new Tracker();
-        new StartUI().init(input, tracker);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = false;
+        while (run) {
+            this.showMenu();
+            System.out.println("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.println("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if () {
+
+            } else if (select == 6) {
+                run = false;
+            }
+        }
     }
 
-    public void init(Input input, Tracker tracker) {
+    private void showMenu() {
+        System.out.println("Menu. ");
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+        System.out.println("Select: ");
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
