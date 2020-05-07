82238fba574d5597ac4780a9a11be043767373a2
Алексей <avsh24@yandex.ru>
2020-05-07 20:18:38 +0500

    0.5. Иерархия исключений и множественный catch.[#252949]

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -3,7 +3,6 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-
         for (int index = 0; index < value.length; index++) {
             if (value[index].equals(key)) {
                 rsl = index;
@@ -17,7 +16,12 @@ public class FindEl {
     }
 
     public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
-        // if contains throw ElementAbuseException;
+        for (int index = 0; index < abuses.length; index++) {
+            if (abuses[index].equals(value)) {
+                return false;
+            }
+        }
+         //if contains throw ElementAbuseException;
         return true;
     }
 
--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -2,10 +2,27 @@ package ru.job4j.ex;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < users.length; index++) {
+            if (users.equals(login)) {
+                rsl = index;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new UserNotFoundException();
+        }
         return null;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
+        boolean rsl = true;
+        if (user.getUsername().equals(user)) {
+            rsl = false;
+        }
+        if (rsl == user.isValid()) {
+            throw new UserInvalidException();
+        }
         return false;
     }
 
