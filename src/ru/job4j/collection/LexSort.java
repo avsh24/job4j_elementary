eec524755a02fd8709c6edbb34bad8326b8173ee
Алексей <avsh24@yandex.ru>
2020-06-02 22:19:09 +0500

    4. Сортировка номера[221284#253110]

--- src/ru/job4j/collection/LexSort.java
+++ src/ru/job4j/collection/LexSort.java
@@ -5,6 +5,14 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
+        String[] l = left.split("\\.");
+        String[] r = right.split("\\.");
+        int size = Math.min(left.length(), right.length());
+        for (int index = 0; index < size; index++) {
+            if (l != r) {
+                break;
+            }
+        }
         if (left.length() > right.length()) {
             return 1;
         } else if (left.length() < right.length()) {
--- src/ru/job4j/collection/LexSortTest.java
+++ src/ru/job4j/collection/LexSortTest.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import java.util.Arrays;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
