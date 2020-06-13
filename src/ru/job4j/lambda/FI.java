629fb14767d66e187003e151a04768bccd8dae29
Алексей <avsh24@yandex.ru>
2020-06-13 13:16:05 +0500

    2. Функциональный интерфейс[247497#252894]

--- src/ru/job4j/lambda/FI.java
+++ src/ru/job4j/lambda/FI.java
@@ -1,7 +1,9 @@
 package ru.job4j.lambda;
 
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class FI {
     public static void main(String[] args) {
@@ -18,5 +20,18 @@ public class FI {
         };
         Arrays.sort(atts, comparator);
         System.out.println(atts);
+    }
+    public static void raw(List<Attachment> list, Function<Attachment, InputStream> func) {
+        for (Attachment att : list) {
+            func.apply(att);
+        }
+        Function<Attachment, InputStream> comparator = (left, right) -> {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getSize() - right.getSize();
+            }
+        };
+        Arrays.sort(list, func);
+        System.out.println(list);
     }
 }
