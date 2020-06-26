7aa8c7c1350504887d85cecf0737364ea880d6f7
Admin <avsh24@yandex.ru>
2020-06-26 15:30:32 +0500

    5. Подсчет функции в диапазоне.[24251#252888]

---
+++ src/ru/job4j/lambda/DiapasonFunc.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiapasonFunc {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>();
+        for (int index = start; index < end; index++) {
+            double x = func.apply((double) index);
+            result.add(x);
+        }
+        return result;
+    }
+}
