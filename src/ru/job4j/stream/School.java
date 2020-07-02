17c44a7f6521960e8f782f2391b941d983e7b08d
Admin <avsh24@yandex.ru>
2020-07-02 12:53:30 +0500

    4. Преобразование List в Map.[110226#252905]

--- src/ru/job4j/stream/School.java
+++ src/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,5 +11,13 @@ public class School {
                 .filter(predict)
                 .collect(Collectors.toList());
         return rsl;
+    }
+
+    Map<String, Student> listToMap(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        e -> e
+                ));
     }
 }
